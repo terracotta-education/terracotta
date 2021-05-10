8b3db00c5697481a350fac747883f43f2bff3a73
Curtis Wolfenberger <curtis.wolfenberger@gmail.com>
2021-05-10 10:56:22 -0700

    Set up Vue, vue-router, Babel, & Sass

---
+++ src/main/javascript/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "../resources/static/app"),
+    indexPath: path.resolve(__dirname, "../resources/static/app/app.html"),
+    assetsDir: "./"
+}
