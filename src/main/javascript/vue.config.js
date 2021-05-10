4e9fd85e3b4059beebe37905deffb2e1f86b60fd
Curtis Wolfenberger <curtis.wolfenberger@gmail.com>
2021-05-10 12:18:08 -0700

    Add Vuetify

--- src/main/javascript/vue.config.js
+++ src/main/javascript/vue.config.js
@@ -3,5 +3,9 @@ const path = require("path");
 module.exports = {
     outputDir: path.resolve(__dirname, "../resources/static/app"),
     indexPath: path.resolve(__dirname, "../resources/static/app/app.html"),
-    assetsDir: "./"
+    assetsDir: "./",
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 }
