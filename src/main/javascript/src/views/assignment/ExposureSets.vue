134afa96df9ae9801b5d2abe230b0030719daf36
Malav Shah <mshah@unicon.net>
2021-06-14 11:34:25 -0700

    Feature/Assignment-Introduction-Component

---
+++ src/main/javascript/src/views/assignment/ExposureSets.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      Assignment Exposure Sets
+    </h1>
+    <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua."
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "AssignmentExposureSets",
+};
+</script>
---
+++ src/main/javascript/src/views/assignment/Intro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>
+      In this section, you'll set up your experiment assignments
+    </h1>
+    <p>
+      Terracotta will populate Canvas assignments with learning activities and
+      materials that change depending on who's looking at them, automatically
+      managing experimental variation within the buckets.
+    </p>
+    <p>
+      All you need to do is create your assignments and specify which treatments
+      will be contained with each assignment. From your student's perspective,
+      they'll be completing assignments as normal within Canvas, with no outward
+      appearance that the assignment is different from any other assignment.
+    </p>
+    <v-btn
+      elevation="0"
+      color="primary"
+      :to="{ name: 'AssignmentExposureSets' }"
+      >Continue</v-btn
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AssignmentIntro",
+};
+</script>
