b5f2c1443c21945bb0fecb87f7390d2573f40ac0
Josh-Thompson <jjjosh@cox.net>
2021-06-16 13:42:45 -0700

    PUT a list of Participants

--- src/main/java/edu/iu/terracotta/service/app/ParticipantService.java
+++ src/main/java/edu/iu/terracotta/service/app/ParticipantService.java
@@ -2,6 +2,7 @@ package edu.iu.terracotta.service.app;
 
 import edu.iu.terracotta.exceptions.DataServiceException;
 import edu.iu.terracotta.exceptions.ParticipantNotUpdatedException;
+import edu.iu.terracotta.model.app.Experiment;
 import edu.iu.terracotta.model.app.Participant;
 import edu.iu.terracotta.model.app.dto.ParticipantDto;
 import edu.iu.terracotta.model.oauth2.SecurityInfo;
@@ -31,4 +32,8 @@ public interface ParticipantService {
     boolean participantBelongsToExperiment(Long experimentId, Long participantId);
 
     void prepareParticipation(Long experimentId, SecurityInfo securityInfo) throws ParticipantNotUpdatedException;
+
+    void changeParticipant(Participant participantToChange, ParticipantDto participantDto, Long experimentId);
+
+    void saveAllParticipants(List<Participant> participantList);
 }
