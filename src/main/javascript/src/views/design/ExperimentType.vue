4c02897f131d818c97d222ae160c6d2c9bd4b0ba
Malav Shah <mshah@unicon.net>
2021-06-18 13:36:59 -0700

    Bug: Fixed Wrong setting DistributionType and ExperimentType

--- src/main/javascript/src/views/design/ExperimentType.vue
+++ src/main/javascript/src/views/design/ExperimentType.vue
@@ -8,19 +8,19 @@
 
 			<v-expansion-panels class="v-expansion-panels--icon" flat>
 
-				<v-expansion-panel :class="{'v-expansion-panel--selected':experiment.exposureType==='BETWEEN'}">
+				<v-expansion-panel :class="{'v-expansion-panel--selected':experiment.exposureType==='WITHIN'}">
 					<v-expansion-panel-header hide-actions><img src="@/assets/all_conditions.svg" alt="all conditions"> All conditions</v-expansion-panel-header>
 					<v-expansion-panel-content>
 						<p>All students are exposed to every condition, in different orders. This way you can compare how the different conditions affected each individual student. This is called a within-subject design.</p>
-						<v-btn @click="saveType('BETWEEN')" color="primary" elevation="0">Select</v-btn>
+						<v-btn @click="saveType('WITHIN')" color="primary" elevation="0">Select</v-btn>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 
-				<v-expansion-panel :class="{'v-expansion-panel--selected':experiment.exposureType==='WITHIN'}">
+				<v-expansion-panel :class="{'v-expansion-panel--selected':experiment.exposureType==='BETWEEN'}">
 					<v-expansion-panel-header hide-actions><img src="@/assets/one_condition.svg" alt="only one condition"> Only one condition</v-expansion-panel-header>
 					<v-expansion-panel-content>
 						<p>Each student is only exposed to one condition, so that you can compare how the different conditions affected different students. This is called a between-subjects design.</p>
-						<v-btn @click="saveType('WITHIN')" color="primary" elevation="0">Select</v-btn>
+						<v-btn @click="saveType('BETWEEN')" color="primary" elevation="0">Select</v-btn>
 					</v-expansion-panel-content>
 				</v-expansion-panel>
 
@@ -69,9 +69,9 @@ export default {
 						if (response.status === 200) {
 							// report the current step
 							this.reportStep({experimentId, step})
-							if (this.experiment.exposureType==='WITHIN') {
+							if (this.experiment.exposureType==='BETWEEN') {
 								this.$router.push({name:'ExperimentDesignDefaultCondition', params:{experiment: experimentId}})
-							} else if(this.experiment.exposureType==='BETWEEN') {
+							} else if(this.experiment.exposureType==='WITHIN') {
 								this.$router.push({name:'ExperimentDesignSummary', params:{experiment: experimentId}})
 							} else {
 								alert("Select an experiment type")
