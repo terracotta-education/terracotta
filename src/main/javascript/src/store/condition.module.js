e5fabeba57082376c1ae87a5387fb0affa8b6084
Curtis Wolfenberger <curtis.wolfenberger@gmail.com>
2021-06-24 15:01:01 -0700

    update default condition save functionality + formatting

--- src/main/javascript/src/store/condition.module.js
+++ src/main/javascript/src/store/condition.module.js
@@ -1,97 +1,98 @@
-import { conditionService } from '@/services'
+import {conditionService} from '@/services'
 
 const state = {}
 
 const actions = {
-    resetCondition: ({ commit }) => {
-        commit('setCondition', {})
-    },
-    createDefaultConditions:({dispatch}, experimentId) => {
-        const defaultConditions = [
-            {
-                name:'',
-                experiment_experiment_id: experimentId
-            },
-            {
-                name:'',
-                experiment_experiment_id: experimentId
-            },
-        ]
-        dispatch('createConditions', defaultConditions)
-    },
-    createConditions: ({dispatch}, conditions) => {
-        if (conditions.length > 0) {
-            conditions.forEach(condition => {
-                dispatch('createCondition', condition)
-            })
-        }
-    },
-    createCondition: ({commit}, condition) => {
-        return conditionService.create(condition)
-                    .then(condition => {
-                        // commit mutation from experiment module
-                        commit('experiment/setCondition', condition, {root:true})
-                    })
-                    .catch(response => {
-                        console.log('createCondition | catch',{response})
-                    })
-    },
-    updateConditions: ({dispatch}, conditions) => {
-        if (conditions.length > 0) {
-            conditions.forEach(condition => {
-                if (condition?.conditionId) {
-                    dispatch('updateCondition', condition)
-                }
-            })
-            return {status: 200}
-        }
-    },
-    updateCondition: ({commit}, condition) => {
-        return conditionService.update(condition)
-                    .then(condition => {
-                        // commit mutation from experiment module
-                        if (condition?.conditionId) {
-                            commit('experiment/updateCondition', condition, {root:true})
-                        }
-                    })
-                    .catch(response => {
-                        console.log('setCondition | catch',{response})
-                    })
-    },
-    setDefaultCondition({dispatch}, payload) {
-        if (!payload || !payload.conditions || !payload.defaultConditionId) {
-            return false
-        }
-        payload.conditions.forEach(condition => {
-            condition.defaultCondition = (condition.conditionId===payload.defaultConditionId)?1:0
-            dispatch('updateCondition', condition)
-            return condition
-        })
-        // Needs to be asynchronous, but forEach doesn't support this yet.
-        // Possibly build out a promise/response
-        return {status: 200}
-    },
-    deleteCondition: ({commit}, condition) => {
-        return conditionService.delete(condition)
-                    .then(() => {
-                        // commit mutation from experiment module
-                        commit('experiment/deleteCondition', condition, {root:true})
-                    })
-                    .catch(response => {
-                        console.log('setCondition | catch',{response})
-                    })
+  resetCondition: ({commit}) => {
+    commit('setCondition', {})
+  },
+  createDefaultConditions: ({dispatch}, experimentId) => {
+    const defaultConditions = [
+      {
+        name: '',
+        experiment_experiment_id: experimentId
+      },
+      {
+        name: '',
+        experiment_experiment_id: experimentId
+      },
+    ]
+    dispatch('createConditions', defaultConditions)
+  },
+  createConditions: ({dispatch}, conditions) => {
+    if (conditions.length > 0) {
+      conditions.forEach(condition => {
+        dispatch('createCondition', condition)
+      })
     }
+  },
+  createCondition: ({commit}, condition) => {
+    return conditionService.create(condition)
+            .then(condition => {
+              // commit mutation from experiment module
+              commit('experiment/setCondition', condition, {root: true})
+            })
+            .catch(response => {
+              console.log('createCondition | catch', {response})
+            })
+  },
+  updateConditions: ({dispatch}, conditions) => {
+    if (conditions.length > 0) {
+      conditions.forEach(condition => {
+        if (condition?.conditionId) {
+          dispatch('updateCondition', condition)
+        }
+      })
+      return {status: 200}
+    }
+  },
+  updateCondition: ({commit}, condition) => {
+    return conditionService.update(condition)
+            .then((response) => {
+              if (response.status === 200) {
+                // commit mutation from experiment module
+                commit('experiment/setCondition', condition, {root: true})
+              }
+            })
+            .catch(response => {
+              console.log('setCondition | catch', {response})
+            })
+  },
+  setDefaultCondition({dispatch}, payload) {
+    if (!payload || !payload.conditions || !payload.defaultConditionId) {
+      return false
+    }
+
+    return Promise.all(
+      payload.conditions.map(async (condition) => {
+        return new Promise((resolve) => {
+          condition.defaultCondition = (condition.conditionId === payload.defaultConditionId) ? 1 : 0
+          dispatch('updateCondition', condition)
+          resolve(condition)
+        });
+      })
+    )
+  },
+  deleteCondition: ({commit}, condition) => {
+    return conditionService.delete(condition)
+            .then(() => {
+              // commit mutation from experiment module
+              commit('experiment/deleteCondition', condition, {root: true})
+            })
+            .catch(response => {
+              console.log('setCondition | catch', {response})
+            })
+  }
 }
 
-const mutations = {
-}
+const mutations = {}
 
 const getters = {}
 
 export const condition = {
-    namespaced: true,
-    state,
-    actions,
-    mutations,
-    getters
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters
 }
--- src/main/javascript/src/views/design/DefaultCondition.vue
+++ src/main/javascript/src/views/design/DefaultCondition.vue
@@ -1,95 +1,95 @@
 <template>
-	<div>
-		<h1>Select the default condition for your experiment</h1>
-		<p>This is the condition students will receive if they do not consent to participate in the experiment or you mark them to be excluded.</p>
+  <div>
+    <h1>Select the default condition for your experiment</h1>
+    <p>This is the condition students will receive if they do not consent to participate in the experiment or you mark
+      them to be excluded.</p>
 
-		<form
-			@submit.prevent="saveConditions"
-			class="my-5"
-			v-if="experiment"
-		>
-
-			<fieldset class="rounded-lg p-5 mb-7" v-if="experiment.conditions">
-				<label  v-for="condition in experiment.conditions" :key="condition.conditionId" :for="`condition-${condition.conditionId}`">
-				<span>{{ condition.name }}</span>
-					<span class="radio-check" >
-						<input name="selectedDefault" v-model="selectedDefault" :value="condition.conditionId" :id="`condition-${condition.conditionId}`" type="radio" required />
+    <form
+      @submit.prevent="saveConditions"
+      class="my-5"
+      v-if="experiment"
+    >
+      <fieldset class="rounded-lg p-5 mb-7" v-if="experiment.conditions">
+        <label v-for="condition in experiment.conditions" :key="condition.conditionId"
+               :for="`condition-${condition.conditionId}`">
+          <span>{{ condition.name }}</span>
+          <span class="radio-check">
+						<input type="radio" name="selectedDefault" v-model="selectedDefault" :value="condition.conditionId"
+                   :id="`condition-${condition.conditionId}`" @change="saveConditions" required/>
 						<span class="rounded-pill px-3 py-1">
 							<v-icon v-show="selectedDefault === condition.conditionId">mdi-check</v-icon>
 							<span>Default</span>
 						</span>
 					</span>
-				</label>
-			</fieldset>
+        </label>
+      </fieldset>
 
-			<v-btn
-				:disabled="!selectedDefault"
-				elevation="0"
-				color="primary"
-				class="mr-4"
-				type="submit"
-			>
-				Next
-			</v-btn>
-		</form>
+      <v-btn
+        :disabled="!selectedDefault"
+        :to="{name: 'ExperimentDesignSummary', params:{experiment: experiment.experimentId}}"
+        elevation="0"
+        color="primary"
+        class="mr-4"
+      >
+        Next
+      </v-btn>
+    </form>
 
-		<v-card
-			class="mt-15 pt-5 px-5 mx-auto blue lighten-5 rounded-lg"
-			outlined
-		>
-			<p><strong>Note:</strong> It's important to specify a default condition so that we know what version of assignments students should receive if they're not participating in the experiment.  This condition should be similar to business-as-usual; the sort of assignment that students would complete during the normal conduct of the course.</p>
-		</v-card>
-	</div>
+    <v-card
+      class="mt-15 pt-5 px-5 mx-auto blue lighten-5 rounded-lg"
+      outlined
+    >
+      <p><strong>Note:</strong> It's important to specify a default condition so that we know what version of
+        assignments students should receive if they're not participating in the experiment. This condition should be
+        similar to business-as-usual; the sort of assignment that students would complete during the normal conduct of
+        the course.</p>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import {mapActions} from "vuex";
 
 export default {
-	name: 'DefaultCondition',
-	props: ['experiment'],
-	data(){
-		return {
-			inputConditionId: null
-		}
-	},
-	computed: {
-		selectedDefault: {
-			get () {
-				const defaultCondition = this.experiment?.conditions.find(condition => condition.defaultCondition === true)
+  name: 'DefaultCondition',
+  props: ['experiment'],
+  data() {
+    return {
+      inputConditionId: null
+    }
+  },
+  computed: {
+    selectedDefault: {
+      get() {
+        const defaultCondition = this.experiment?.conditions.find(condition => condition.defaultCondition === true)
 
-				if (this.inputConditionId !== null) {
-					return this.inputConditionId
-				} else if(defaultCondition) {
-					return defaultCondition.conditionId
-				} else {
-					return true
-				}
-			},
-			set (val) {
-				this.inputConditionId = val
-			}
-		}
-	},
-	methods: {
-		...mapActions({
-			setDefaultCondition: 'condition/setDefaultCondition',
-		}),
-		saveConditions() {
-			const conditions = this.experiment.conditions
-			const defaultConditionId = this.selectedDefault
+        if (this.inputConditionId !== null) {
+          return this.inputConditionId
+        } else if (defaultCondition) {
+          return defaultCondition.conditionId
+        } else {
+          return true
+        }
+      },
+      set(val) {
+        this.inputConditionId = val
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      setDefaultCondition: 'condition/setDefaultCondition',
+    }),
+    saveConditions() {
+      const conditions = this.experiment.conditions
+      const defaultConditionId = this.selectedDefault
 
-			this.setDefaultCondition({conditions, defaultConditionId})
-					.then(response => {
-						if (response?.status === 200) {
-							this.$router.push({name:'ExperimentDesignSummary', params:{experiment: this.experiment.experimentId}})
-						}
-					})
-					.catch(response => {
-						console.log("catch", {response})
-					})
-		},
-	}
+      this.setDefaultCondition({conditions, defaultConditionId})
+          .catch(response => {
+            console.log("catch", {response})
+          })
+    },
+  }
 }
 </script>
 
@@ -97,49 +97,51 @@ export default {
 @import '~@/styles/variables';
 
 fieldset {
-	padding:10px 15px 10px 20px;
-	border: 1px solid map-get($grey, 'lighten-2');
+  padding: 10px 15px 10px 20px;
+  border: 1px solid map-get($grey, 'lighten-2');
 
-	> label {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		font-size: 16px;
+  > label {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 16px;
 
-		&:not(:last-child) {
-			//margin-bottom: 15px;
-			border-bottom: 1px solid map-get($grey, 'lighten-2');
-		}
-		> span {
-			display: block;
-			padding: 15px 0;
-		}
-	}
+    &:not(:last-child) {
+      border-bottom: 1px solid map-get($grey, 'lighten-2');
+    }
+
+    > span {
+      display: block;
+      padding: 15px 0;
+    }
+  }
 }
+
 .radio-check {
-	font-size: 14px;
+  font-size: 14px;
 
-	input[type="radio"] {
-		display: none;
+  input[type="radio"] {
+    display: none;
 
-		+ span {
-			display: block;
-			background-color: map-get($grey, 'lighten-2');
-			padding: 15px 0;
-		}
-		&:checked {
-			+ span {
-				background-color: map-get($light-blue, 'base');
-				color: white;
+    + span {
+      display: block;
+      background-color: map-get($grey, 'lighten-2');
+      padding: 15px 0;
+    }
 
-				.v-icon {
-					color: white;
-					margin-right: 5px;
-					font-size: 18px;
-					vertical-align: text-bottom;
-				}
-			}
-		}
-	}
+    &:checked {
+      + span {
+        background-color: map-get($light-blue, 'base');
+        color: white;
+
+        .v-icon {
+          color: white;
+          margin-right: 5px;
+          font-size: 18px;
+          vertical-align: text-bottom;
+        }
+      }
+    }
+  }
 }
 </style>
