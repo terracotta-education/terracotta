96622937cf8022dd3edf926c6987393dc6683597
Malav Shah <mshah@unicon.net>
2021-06-29 14:29:34 -0700

    Updated ParticipationManualDistribution to fetch and work on real data

--- src/main/javascript/src/store/exposures.module.js
+++ src/main/javascript/src/store/exposures.module.js
@@ -1,33 +1,33 @@
-import { exposuresService } from '@/services'
+import { exposuresService } from "@/services";
 
 const state = {
   exposures: null,
-}
+};
 
 const actions = {
   fetchExposures: ({ commit }, experimentId) => {
     return exposuresService
       .getAll(experimentId)
       .then((data) => {
-        commit('setExposuresService', data)
+        commit('setExposuresService', data);
       })
       .catch((response) => {
-        console.log('fetchExposures | catch', { response })
-      })
+        console.log('fetchExposures | catch', { response });
+      });
   },
-}
+};
 
 const mutations = {
   setExposuresService(state, data) {
-    state.exposures = data
+    state.exposures = data;
   },
-}
+};
 
 const getters = {
   exposures(state) {
-    return state.exposures
+    return state.exposures;
   },
-}
+};
 
 export const exposures = {
   namespaced: true,
@@ -35,4 +35,4 @@ export const exposures = {
   actions,
   mutations,
   getters,
-}
+};
--- src/main/javascript/src/views/participation/distribution/ParticipationManualDistribution.vue
+++ src/main/javascript/src/views/participation/distribution/ParticipationManualDistribution.vue
@@ -12,11 +12,11 @@
         :key="condition.conditionId"
       >
         <v-expansion-panel-header>
-          {{ condition.name }} (0)
+          {{ condition.name }} ({{ arrayDataProxy[index].length }})
         </v-expansion-panel-header>
         <v-expansion-panel-content>
           <ListParticipants
-            :listOfParticipants="arrayData[index]"
+            :listOfParticipants="arrayDataProxy[index]"
             :moveToOptions="getConditionNames"
             :moveToHandler="moveToHandler"
             :selectedOption="'' + index"
@@ -27,30 +27,27 @@
 
     <!-- Unassigned -->
     <ListParticipants
-      :listOfParticipants="arrayData[this.getConditionNames.length - 1]"
+      :listOfParticipants="arrayDataProxy[this.getConditionNames.length - 1]"
       :moveToOptions="getConditionNames"
       :moveToHandler="moveToHandler"
       :selectedOption="'' + (this.getConditionNames.length - 1)"
     />
 
-    <!-- {{ this.arrayData[2] }}
-    <br />
-    {{ this.conditions }}
-    <br />
-    {{ this.participants }}
-    <br />
-
-    {{ this.experiment }}
-
-    <br /> -->
-    <v-btn elevation="0" class="mt-3" color="primary">Continue</v-btn>
+    <v-btn
+      elevation="0"
+      class="mt-10"
+      color="primary"
+      @click="submitDistribution"
+      >Continue</v-btn
+    >
   </div>
 </template>
 
 <script>
-import store from "@/store";
-import { mapGetters } from "vuex";
 import ListParticipants from "../../../components/ListParticipants.vue";
+import store from "@/store";
+import { mapGetters, mapActions } from "vuex";
+import { participantService } from "@/services";
 
 export default {
   name: "ParticipationManualDistribution",
@@ -60,12 +57,31 @@ export default {
   },
   data() {
     return {
-      //   arrayD: [[], [], [...this.participants]],
+      arrayDataProxy: [],
     };
+  },
+  watch: {
+    participants: {
+      // Watcher is required for keeping track of changes
+      // made in Participants
+      deep: true,
+      immediate: true,
+      handler(newValue) {
+        // This will only required when the page is loaded
+        const newArray = [];
+        for (let i = 0; i < this.conditions.length; i++) {
+          newArray.push([]);
+        }
+        // All the participant will go to 'Unparticipate' section
+        newArray.push(newValue);
+        this.arrayData = newArray;
+      },
+    },
   },
   computed: {
     ...mapGetters({
       participants: "participants/participants",
+      exposures: "exposures/exposures",
     }),
 
     conditions() {
@@ -78,64 +94,117 @@ export default {
         "Unassigned",
       ];
     },
-    // arrayData() {
-    //   return (index) => {
-    //     console.log(index);
-    //     return [[],[],this.participants];
-    //   };
-    // },
+
     arrayData: {
-      get: function(index) {
+      get: function() {
         console.log("In Get");
-        const _this = this;
-        console.log(index);
-        return [[], [], [..._this.participants]];
+        const newArray = [];
+        for (let i = 0; i < this.conditions.length; i++) {
+          newArray.push([]);
+        }
+        // All the participant will go to 'Unparticipate' section
+        newArray.push(this.participants);
+        return newArray;
       },
       set: function(newValue) {
-        console.log("newValue: ", newValue);
-        return newValue;
+        this.arrayDataProxy = newValue;
       },
-    },
-
-    defaultParticipantsToConditions() {
-      const temp = [];
-      this.participants.forEach(() => {
-        temp.push([]);
-      });
-      return [...temp, this.participants];
     },
   },
   methods: {
-    getParticipants(index) {
-      return this.defaultParticipantsToConditions[index]
-        ? this.defaultParticipantsToConditions[index]
-        : this.defaultParticipantsToConditions;
+    ...mapActions({
+      fetchExposures: "exposures/fetchExposures",
+    }),
+
+    getExposure() {
+      this.fetchExposures(this.experiment.experimentId);
+    },
+
+    submitDistribution() {
+      const requestBody = [];
+      const conditionGroupIDMap = {};
+
+      this.getExposure();
+      const firstExposureId = this.exposures
+        .map((expo) => expo.exposureId)
+        .sort((a, b) => a - b)[0];
+
+      const firstExposure = this.exposures.filter(
+        (expo) => expo.exposureId === firstExposureId
+      )[0];
+
+      firstExposure.groupConditionList.map(
+        ({ groupId }, index) => (conditionGroupIDMap[index] = groupId)
+      );
+
+      this.arrayDataProxy.map((arrData, index) =>
+        arrData.map((participant) => {
+          if (conditionGroupIDMap[index] !== undefined) {
+            const temp = {
+              participantId: participant.participantId,
+              consent: participant.consent,
+              dropped: participant.dropped,
+              groupId: conditionGroupIDMap[index],
+            };
+            requestBody.push(temp);
+          }
+        })
+      );
+
+      participantService
+        .updateParticipants(this.experiment.experimentId, requestBody)
+        .then((response) => {
+          if (response?.status === 200) {
+            this.$router.push({
+              name: "ParticipationSummary",
+              params: { experiment: this.experiment.experimentId },
+            });
+          } else {
+            alert(response.error);
+          }
+        })
+        .catch((response) => {
+          console.log("submitParticipants | catch", { response });
+        });
     },
 
     moveToHandler(option, tempSelected) {
-      console.log("Here", option, JSON.stringify(tempSelected));
+      const selectedParticipantIDs = tempSelected.map(
+        (participant) => participant.participantId
+      );
 
-      this.arrayData = [[...this.participants], [], []];
-      //   this.title = "Hello2";
+      const filteredParticipants = this.arrayDataProxy.map(
+        (conditionParticipantMap) =>
+          conditionParticipantMap.filter(
+            (participant) =>
+              !selectedParticipantIDs.includes(participant.participantId)
+          )
+      );
+
+      const idx = this.getConditionNames.indexOf(option);
+      filteredParticipants[idx] = [
+        ...filteredParticipants[idx],
+        ...tempSelected,
+      ];
+
+      this.arrayData = filteredParticipants;
     },
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-  updated() {
-    console.log("update");
   },
   beforeRouteEnter(to, from, next) {
     // don't load new data after participant selection screen
-    return store
-      .dispatch("participants/fetchParticipants", to.params.experiment_id)
-      .then(next, next);
+    return (
+      store
+        .dispatch("participants/fetchParticipants", to.params.experiment_id)
+        .then(next, next)
+    );
   },
   beforeRouteUpdate(to, from, next) {
     // don't load new data after participant selection screen
-    return store
-      .dispatch("participants/fetchParticipants", to.params.experiment_id)
-      .then(next, next);
+    return (
+      store
+        .dispatch("participants/fetchParticipants", to.params.experiment_id)
+        .then(next, next)
+    );
   },
 };
 </script>
