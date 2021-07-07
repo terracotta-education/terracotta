0e6d37349a0b2bee589d15a6f9101bad2a09d22f
Curtis Wolfenberger <curtis.wolfenberger@gmail.com>
2021-07-07 10:04:51 -0700

    Add deeper file support, add consent file template to consent screen

---
+++ src/main/resources/static/app/js/chunk-2d0aab08.ba548f8b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aab08"],{"11a3":function(n,e,t){"use strict";t.r(e);var r=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},o=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h1",[n._v(" Add your treatment for Assignment 1’s condition: "),t("strong",[n._v("Biology of skin color")])])])}],a={name:"TerracottaBuilder"},c=a,i=t("2877"),s=Object(i["a"])(c,r,o,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0aab08.ba548f8b.js.map
