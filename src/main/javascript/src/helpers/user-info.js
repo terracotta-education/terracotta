39a0e2dca7cf521829b3a37882e0c70a65d77425
Malav Shah <mshah@unicon.net>
2021-08-20 14:26:22 -0700

    Update userInfo logic to detect the user

--- src/main/javascript/src/helpers/user-info.js
+++ src/main/javascript/src/helpers/user-info.js
@@ -1,4 +1,4 @@
 export function userInfo(roles) {
-    const values = roles.map((role) => role.split('#').includes('Instructor')) 
+    const values = roles.map((role) => role.split('/').includes('membership#Instructor')) 
     return values.some((value) => value) ? 'Instructor' : 'Learner'
 }
