bb1478d277b73fc59d60836a63813fb1ae68881e
Bob Long <relong76@gmail.com>
2022-09-14 12:57:45 -0400

    TCOTA-517 TCOTA-536 updating data runner assignment date fix for batch performance

--- src/main/java/edu/iu/terracotta/repository/AssignmentRepository.java
+++ src/main/java/edu/iu/terracotta/repository/AssignmentRepository.java
@@ -1,6 +1,9 @@
 package edu.iu.terracotta.repository;
 
 import edu.iu.terracotta.model.app.Assignment;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -21,6 +24,8 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
     boolean existsByExposure_Experiment_ExperimentIdAndAssignmentId(Long experimentId, Long assignmentId);
 
+    Page<Assignment> findAll(Pageable pageable);
+
     @Transactional
     @Modifying
     @Query("delete from Assignment s where s.assignmentId = ?1")
@@ -31,12 +36,5 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
     @Query("select a from Assignment a where a.exposure.experiment.ltiContextEntity.contextId = ?1 and a.exposure.experiment.closed is null and a.assignmentId NOT IN (Select a2.assignmentId from Assignment a2 where a2.softDeleted = true)")
     List<Assignment> findAssignmentsToCheckByContext(long contextId);
-
-
-
-
-
-
-
 
 }
