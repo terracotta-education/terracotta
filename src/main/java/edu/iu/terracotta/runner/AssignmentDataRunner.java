d9ed632a04180ea7c244c7649e05733bb7e4e414
Bob Long <relong76@gmail.com>
2022-09-14 09:16:15 -0400

    TCOTA-517 TCOTA-536 updating data runner assignment date fix for batch performance

--- src/main/java/edu/iu/terracotta/runner/AssignmentDataRunner.java
+++ src/main/java/edu/iu/terracotta/runner/AssignmentDataRunner.java
@@ -1,16 +1,18 @@
 package edu.iu.terracotta.runner;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import edu.iu.terracotta.model.app.Assignment;
 import edu.iu.terracotta.repository.AllRepositories;
@@ -24,8 +26,11 @@ public class AssignmentDataRunner implements ApplicationListener<ApplicationRead
     @Autowired
     private AllRepositories allRepositories;
 
-    @Value("${app.assignments.fix.start.dates:false}")
+    @Value("${app.assignments.fix.start.dates.enabled:false}")
     private boolean enabled;
+
+    @Value("${app.assignments.fix.start.dates.batchsize:100}")
+    private int batchSize;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
@@ -34,22 +39,30 @@ public class AssignmentDataRunner implements ApplicationListener<ApplicationRead
         }
 
         // fix legacy assignment started date
-        List<Assignment> assignments = allRepositories.assignmentRepository.findAll();
+        int page = 0;
+        Page<Assignment> assignments = allRepositories.assignmentRepository.findAll(PageRequest.of(page++, batchSize));
 
         log.info("Starting assignment start date fix...");
+        int processed = 0;
 
-        assignments.stream()
-            .filter(assignment -> { return !assignment.isStarted(); })
-            .forEach(
-                assignment -> {
-                    long submissionsCount = allRepositories.submissionRepository.countByAssessment_Treatment_Assignment_AssignmentId(assignment.getAssignmentId());
-    
-                    if (submissionsCount > 0) {
-                        assignment.setStarted(Timestamp.valueOf(LocalDateTime.now()));
-                        allRepositories.assignmentRepository.save(assignment);
+        while (CollectionUtils.isNotEmpty(assignments.getContent())) {
+            processed += assignments.getContent().size();
+            assignments.getContent().stream()
+                .filter(assignment -> { return !assignment.isStarted(); })
+                .forEach(
+                    assignment -> {
+                        long submissionsCount = allRepositories.submissionRepository.countByAssessment_Treatment_Assignment_AssignmentId(assignment.getAssignmentId());
+
+                        if (submissionsCount > 0) {
+                            assignment.setStarted(Timestamp.valueOf(LocalDateTime.now()));
+                            allRepositories.assignmentRepository.save(assignment);
+                        }
                     }
-                }
-            );
+                );
+
+            log.info("Processed {} assignment startd date records...", processed);
+            assignments = allRepositories.assignmentRepository.findAll(PageRequest.of(page++, batchSize));
+        }
 
         log.info("Assignment start date fix complete!");
     }
