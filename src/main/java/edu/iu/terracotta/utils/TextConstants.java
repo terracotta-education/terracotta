1e2b6c3c8b124581b69fe1f79eef31d8b6b4ba66
Bob Long <relong76@gmail.com>
2022-09-22 16:57:44 -0400

    TCOTA-493 TCOTA-552 update assignment title on duplicate

--- src/main/java/edu/iu/terracotta/utils/TextConstants.java
+++ src/main/java/edu/iu/terracotta/utils/TextConstants.java
@@ -34,6 +34,7 @@ public class TextConstants {
     public static final String ERROR_DEEP_RESPONSE = "Error creating the DeepLinking Response";
     public static final String NOT_FOUND_SUFFIX = " not found";
     public static final String LTI3ERROR = "lti3Error";
+    public static final String DUPLICATE_PREFIX = "Copy of";
     public static final String NOT_ENOUGH_PERMISSIONS = "Error 104: Not enough permissions to access to this endpoint.";
     public static final String BAD_TOKEN = "The token does not contain the expected information";
     public static final String ID_IN_POST_ERROR = "Error 107: Unable to create. The POST endpoint does not accept objects with id";
