fb26041d3d2da79c092d4645bcc9a94d2f293680
Bob Long <relong76@gmail.com>
2022-09-26 14:31:20 -0400

    TCOTA-493 TCOTA-498 updating export csv functionality

---
+++ src/main/java/edu/iu/terracotta/model/app/enumerator/export/OutcomesCsv.java
@@ -0,0 +1,33 @@
+package edu.iu.terracotta.model.app.enumerator.export;
+
+import java.util.Arrays;
+
+public enum OutcomesCsv {
+
+    // NOTE: order is important!
+    OUTCOME_ID("outcome_id"),
+    PARTICIPANT_ID("participant_id"),
+    EXPOSURE_ID("exposure_id"),
+    SOURCE("source"),
+    OUTCOME_NAME("outcome_name"),
+    POINTS_POSSIBLE("points_possible"),
+    OUTCOME_SCORE("outcome_score");
+
+    public static final String FILENAME = "outcomes.csv";
+
+    private String header;
+
+    private OutcomesCsv(String header) {
+        this.header = header;
+    }
+
+    @Override
+    public String toString() {
+        return header;
+    }
+
+    public static String[] getHeaderRow() {
+        return Arrays.stream(OutcomesCsv.values()).map(OutcomesCsv::toString).toArray(String[]::new);
+    }
+
+}
