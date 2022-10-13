5843566dba04a01dd936ef8e976a26a03d7f0863
Bob Long <relong76@gmail.com>
2022-10-13 17:19:35 -0400

    TCOTA-568 add retake not allowed messaging (backend)

--- src/main/java/edu/iu/terracotta/model/app/RetakeDetails.java
+++ src/main/java/edu/iu/terracotta/model/app/RetakeDetails.java
@@ -1,10 +1,13 @@
 package edu.iu.terracotta.model.app;
 
+import edu.iu.terracotta.utils.TextConstants;
+
 public class RetakeDetails {
 
     private boolean retakeAllowed = false;
     private Float keptScore;
     private Integer submissionAttemptsCount;
+    private String retakeNotAllowedReason;
 
     public boolean isRetakeAllowed() {
         return retakeAllowed;
@@ -28,6 +31,35 @@ public class RetakeDetails {
 
     public void setSubmissionAttemptsCount(Integer submissionAttemptsCount) {
         this.submissionAttemptsCount = submissionAttemptsCount;
+    }
+
+    public String getRetakeNotAllowedReason() {
+        return retakeNotAllowedReason;
+    }
+
+    public void setRetakeNotAllowedReason(String retakeNotAllowedReason) {
+        this.retakeNotAllowedReason = retakeNotAllowedReason;
+    }
+
+    public enum RetakeNotAllowedReason {
+
+        MAX_NUMBER_ATTEMPTS_REACHED, // max # of attempts reached
+        WAIT_TIME_NOT_REACHED, // submission wait time has not passed
+        OTHER, // generic catch-all reason (should not happen)
+
+    }
+
+    public static String calculateRetakeNotAllowedReason(String message) {
+        switch (message) {
+            case TextConstants.LIMIT_OF_SUBMISSIONS_REACHED:
+                return RetakeNotAllowedReason.MAX_NUMBER_ATTEMPTS_REACHED.toString();
+
+            case TextConstants.ASSIGNMENT_SUBMISSION_WAIT_TIME_NOT_REACHED:
+                return RetakeNotAllowedReason.WAIT_TIME_NOT_REACHED.toString();
+
+            default:
+                return RetakeNotAllowedReason.OTHER.toString();
+        }
     }
 
 }
