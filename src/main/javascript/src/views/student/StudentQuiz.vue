ed67df86d7271794fcb90887999d526c38a4b6c8
Ryan Mathis <rmathis@unicon.net>
2022-10-17 09:35:48 -0700

    TCOTA-574 add submission details by default

--- src/main/javascript/src/views/student/StudentQuiz.vue
+++ src/main/javascript/src/views/student/StudentQuiz.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid v-if="!loading">
-    <v-row v-if="readonly">
+    <v-row>
       <v-col v-if="canTryAgain">
         <v-btn
           @click="handleTryAgain"
@@ -20,7 +20,7 @@
         </p>
       </v-col>
       <v-spacer />
-      <v-col v-if="!muted">
+      <v-col v-if="showSubmissionDetails">
         <h2>Submission Details</h2>
         <v-divider />
           <v-list dense flat>
@@ -271,7 +271,10 @@ export default {
       return this.questionPageIndex < this.questionPages.length - 1;
     },
     canTryAgain() {
-      return this.assignmentData ? this.assignmentData.retakeDetails.retakeAllowed : false;
+      return this.readonly && (this.assignmentData ? this.assignmentData.retakeDetails.retakeAllowed : false);
+    },
+    showSubmissionDetails() {
+      return this.readonly || this.submitted;
     },
     selectedSubmissionDateSubmitted() {
       return moment(this.selectedSubmission?.dateSubmitted).format('MMMM Do YYYY hh:mm');
@@ -283,7 +286,7 @@ export default {
     currentScore() {
       let grade;
       if (!this.selectedSubmission) {
-        grade = '-';
+        grade = this.assignmentData?.retakeDetails.keptScore;
       } else {
         const { totalAlteredGrade, alteredCalculatedGrade } = this.selectedSubmission;
         grade = totalAlteredGrade !== null ? totalAlteredGrade : alteredCalculatedGrade;
@@ -450,7 +453,14 @@ export default {
           throw Error("Error submitting quiz: " + data);
         }
 
-        this.submitted = true;
+        const view = await this.viewAssignment();
+
+        if (view?.status === 200) {
+          const { data } = view;
+          this.assignmentData = data;
+          this.submitted = true;
+        }
+
       } catch (e) {
         console.error({ e });
         throw e; // rethrow
@@ -533,6 +543,11 @@ export default {
         this.$refs.form.scrollIntoView({ behavior: "smooth" });
       });
     },
+    async viewAssignment() {
+      const experimentId = this.experimentId;
+      const step = "view_assignment";
+      return this.reportStep({ experimentId, step });
+    },
     async attempt() {
       const experimentId = this.experimentId;
       const step = "launch_assignment";
@@ -567,19 +582,17 @@ export default {
     },
   },
   async created() {
-    const experimentId = this.experimentId;
-    const step = "view_assignment";
     this.loading = true;
     try {
-      const stepResponse = await this.reportStep({ experimentId, step });
+      const stepResponse = await this.viewAssignment();
 
       if (stepResponse?.status === 200) {
         const { data } = stepResponse;
         this.assignmentData = data;
 
-        const { submissions } = data;
-        // const { retakeAllowed } = retakeDetails;
-        if (submissions.length > 0) {
+        const { retakeDetails } = data;
+        const { retakeAllowed } = retakeDetails;
+        if (!retakeAllowed) {
           this.readonly = true;
         } else {
           this.attempt();
